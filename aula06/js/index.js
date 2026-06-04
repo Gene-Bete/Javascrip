4e136d1d2ad77b90181621c532761b3eb7128ffa
Gene-Bete <genecavelebete@gmail.com>
2026-06-04 16:36:57 +0200

    atulizei a aula6

--- aula06/js/index.js
+++ aula06/js/index.js
@@ -2,3 +2,21 @@ function eventoClink(){
     alert('acionou um evento de clink');
     document.body.style.backgroundColor = "yellow";
 }
+function eventoDblClink(){
+    alert('evento de clique duplo');
+}
+function viraVermelho(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "red";
+}
+function viraAzul(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "blue";
+}
+function adicionaTexto(){
+    let p = document.getElementById("texto"); 
+    p.append('O mouse moveu<br>'); 
+}
+function mudou(){
+   console.log('Mudou')
+}
