8f9740c55ab973c594edb49ab75cc12deb904ffc
Gene-Bete <genecavelebete@gmail.com>
2026-06-08 21:24:38 +0200

    atualiza a aula07

---
+++ aula07/js/index.js
@@ -0,0 +1,20 @@
+
+var item1 = "arroz";
+var item2 = "feijao";
+var item3 = "macarrao";
+var item4 = "leite";
+              
+ 
+const jogadores= [
+    "Biro Biro", 
+    "Ribamar",
+    "Pele", 
+    "Maradona",
+    "Neymar",
+    "Cristiano Ronaldo",
+    "Dimitri",
+
+]; //array\\
+const jogOrdem = jogadores.sort();
+
+document.getElementById("teste").innerHTML = jogOrdem;
