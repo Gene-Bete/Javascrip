26ffd515cf457305d4e919e766d73cd54f595324
Gene-Bete <genecavelebete@gmail.com>
2026-06-22 18:22:04 +0200

    atualizar a aula 12
    a aula 12 falamos de class

---
+++ aula12/js/index.js
@@ -0,0 +1,21 @@
+function ola() {
+    console.log("olá mundo!")
+}
+
+
+class Carro {
+    constructor(valor1, valor2, valor3){
+        this.marca = valor1;
+        this.modelo = valor2;
+        this.ano = valor3;
+    }
+    buzina () {
+        return this.modelo +  "buzina: Biiiiiiiiii";
+    }
+}
+
+const uno = new Carro("Fiat", "Uno", 2001);
+const gol = new Carro("Volkswagen", "Gol", 2013);
+console.log(gol);
+gol.ano =  2014
+console.log(gol);
